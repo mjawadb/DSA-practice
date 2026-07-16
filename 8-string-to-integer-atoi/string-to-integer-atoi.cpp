c9a95902d4b42b2b73c71dd563b886ccfcda0d73
Muhammad Jawad Bufthem <138138634+mjawadb@users.noreply.github.com>
2026-07-16 18:31:51 +0700

    Time: 1 ms (22.83%) | Memory: 9.2 MB (56.10%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
+// testing letsync
     int myAtoi(string s) {
         int i = 0, sign = 1;
-        long res = 0; // Using long to handle overflow cases
+        long res = 0; 
 
-        // Trim leading spaces
         while (i < s.size() && s[i] == ' ') i++;
         if (i == s.size()) return 0;
 
-        // Check for sign
         if (s[i] == '-') { sign = -1; i++; }
         else if (s[i] == '+') i++;
 
-        // Process numerical characters
         while (i < s.size() && isdigit(s[i])) {
             res = res * 10 + (s[i] - '0');
-
-            // Handle overflow
+            
             if (sign * res > INT_MAX) return INT_MAX;
             if (sign * res < INT_MIN) return INT_MIN;
 
